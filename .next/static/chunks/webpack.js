d246a0e5ee59f8181549aa8368498a7ab5fbe0a9
ufukcoskunn01-maker <ufuk.coskunn01@gmail.com>
2026-03-04 10:22:57 +0300

    Updated app-build-manifest.json

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "eede2cd5c7e0f883"; }
+/******/ 		__webpack_require__.h = function() { return "44008c0717071c9d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
